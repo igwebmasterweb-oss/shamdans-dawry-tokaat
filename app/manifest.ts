7e8f8cba2c9a77df74e78439be5e78cd9b0eace2
igwebmasterweb-oss <i.g.webmaster.web@gmail.com>
2026-06-04 20:54:32 +0300

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,19 +16,19 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ['games', 'sports'],
     icons: [
       {
-        src: '/logo-FF.png',
+        src: '/Shedan_logo.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/logo-FF.png',
+        src: '/Shedan_logo.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/logo-FF.png',
+        src: '/Shedan_logo.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
